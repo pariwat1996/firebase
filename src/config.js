3fa19426a6a47c4b90aeead920d3ed01f694b290
Your Name <woramet2539@icloud.com>
2020-03-19 17:04:24 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+  apiKey: "AIzaSyCnxeBdLkl8d8IHXYMulX5IIftRVdyV46w",
+  authDomain: "todo-ab065.firebaseapp.com",
+  databaseURL: "https://todo-ab065.firebaseio.com",
+  projectId: "todo-ab065",
+  storageBucket: "todo-ab065.appspot.com",
+  messagingSenderId: "308471268913",
+  appId: "1:308471268913:web:96962ce2d7348df4eff710",
+  measurementId: "G-TG5LKDHWX3"
+};
+
+export default config;
